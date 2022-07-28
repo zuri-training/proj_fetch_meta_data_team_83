50ab95e3401944f9d040905b3c544a052b691df9
idimmusix <idimmusix@gmail.com>
2022-07-28 15:25:49 +0100

    faq javascript

--- fetch_metadata/static/index.js
+++ fetch_metadata/static/index.js
@@ -1,17 +1,16 @@
-const faqQuest = document.querySelectorAll("faq_quest");
+const faqQuest = document.querySelectorAll(".faq_quest");
 console.log(faqQuest);
 
-faqQuest.forEach(faqQuest => {
-  faqQuest.addEventListener("click", event=>{
+faqQuest.forEach((faqQuest) => {
+  faqQuest.addEventListener("click", (event) => {
     faqQuest.classList.toggle("active");
-    const faqBody = faqQuest.nextElementSibling
-    console.log(faqBody)
-    if(faqQuest.classList.contains(active)){
-      faqBody.style.maxHeight = faqBody.scrollHeight+"px";
+    const faqBody = faqQuest.nextElementSibling;
+    console.log(faqBody);
+    if (faqQuest.classList.contains("active")) {
+      faqBody.style.maxHeight = faqBody.scrollHeight + "px";
       console.log(faqBody.scrollHeight);
+    } else {
+      faqBody.style.maxHeight = 0;
     }
-    else{
-      faqBody.style.maxHeight=0;
-    }
-  })
-})
+  });
+});
