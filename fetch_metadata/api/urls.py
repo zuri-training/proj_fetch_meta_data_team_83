bd89012baf80d992ed3fcf7ab402a8fb68223d58
idimmusix <idimmusix@gmail.com>
2022-08-01 13:33:42 +0100

    route everything to api

--- fetch_metadata/api/urls.py
+++ fetch_metadata/api/urls.py
@@ -1,13 +1,19 @@
+from django.contrib import admin
 from django.urls import path,include
-from .views import CreateUserView,LoginUserView
 
 from rest_framework.authtoken.views import obtain_auth_token
-from . import views
+
+from .views import CreateUserView,LoginUserView
+
+
 
 app_name = 'api'
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('token/', obtain_auth_token, name='get_token'),
     path('signup/', CreateUserView.as_view(), name='signup'),
     path('login/', LoginUserView.as_view(), name='login'),
+    path('', include('apps.commons.urls')),
+    path('file/', include('apps.file_control.urls')),
 ]
