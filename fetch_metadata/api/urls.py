65ad5c7b813e02b39218461c14ff58f613c64105
idimmusix <idimmusix@gmail.com>
2022-08-02 12:17:18 +0100

    edited the file_control/models and urls

--- fetch_metadata/api/urls.py
+++ fetch_metadata/api/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+
 from django.urls import path,include
 
 from rest_framework.authtoken.views import obtain_auth_token
@@ -10,7 +10,7 @@ from .views import CreateUserView,LoginUserView
 app_name = 'api'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('token/', obtain_auth_token, name='get_token'),
     path('signup/', CreateUserView.as_view(), name='signup'),
     path('login/', LoginUserView.as_view(), name='login'),
--- fetch_metadata/apps/file_control/models.py
+++ fetch_metadata/apps/file_control/models.py
@@ -1,5 +1,3 @@
-
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth import get_user_model
 
