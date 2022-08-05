f4681d178400bbca1a3e2d3b69803418dddf9179
idimmusix <idimmusix@gmail.com>
2022-08-05 20:39:17 +0100

    battery about to die

--- fetch_metadata/fetch_metadata/settings.py
+++ fetch_metadata/fetch_metadata/settings.py
@@ -165,18 +165,22 @@ REST_FRAMEWORK = {
 
 # Configure Django App for Heroku
 if DEBUG is False:
-    django_on_heroku.settings(locals())
-
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
     AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_CUSTOM_DOMAIN = config("AWS_S3_CUSTOM_DOMAIN")
+    AWS_S3_REGION_NAME = "af-south-1"
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
+    AWS_DEFAULT_ACL=None
+
+    AWS_CLOUDFRONT_KEY = config('AWS_CLOUDFRONT_KEY', None).encode('ascii')
+    AWS_CLOUDFRONT_KEY_ID = config('AWS_CLOUDFRONT_KEY_ID', None)
     AWS_LOCATION = 'media'
 
     STATICFILES_DIRS = [
@@ -186,16 +190,6 @@ if DEBUG is False:
     MEDIA_URL = 'https://fetchmetadata.s3.af-south-1.amazonaws.com/media/'
     STATIC_URL = 'https://fetchmetadata.s3.af-south-1.amazonaws.com/static/'
 
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-    DEFAULT_FILE_STORAGE = 'buckets.storage.S3Storage'
-
-    AWS = {
-    'BUCKET': 'fetchmetadata',
-    'ACCESS_KEY': config("AWS_ACCESS_KEY_ID"),
-    'SECRET_KEY': config("AWS_SECRET_ACCESS_KEY"),
-    'REGION': 'af-south-1'
-    }
 else:
     #Media files (uploaded files)
     MEDIA_URL = '/media/'
@@ -210,3 +204,6 @@ else:
         os.path.join(BASE_DIR, 'static')
         ]
 
+django_on_heroku.settings(locals())
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
