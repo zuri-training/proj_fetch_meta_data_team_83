d656bb3fb52d6324f1684e667881f1e5166c2e49
idimmusix <idimmusix@gmail.com>
2022-08-10 08:40:45 +0000

    roll back

--- fetch_metadata/apps/file_control/urls.py
+++ fetch_metadata/apps/file_control/urls.py
@@ -7,6 +7,6 @@ app_name='file'
 urlpatterns = [
     path('',views.FileListView.as_view(), name='userFileList'),
     path('dashboard/',views.FileCreateView.as_view(), name='userFileUpload'),
-    path('<int:pk>/', views.FileDetailView.as_view(), name='fileDetail'),
-    path('test/', views.read_file, name='fileDetail'),
+    path('<int:pk>/', views.FileDetailView.as_view(), name='file_detail'),
+    # path('test/', views.read_file, name='fileDetail'),
 ]
